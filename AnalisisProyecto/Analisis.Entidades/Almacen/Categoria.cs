24dca9678d6c9ea50b1725efad249497bb03a12d
Fernanda-UMG <78993516+Fernanda-UMG@users.noreply.github.com>
2021-05-19 22:52:46 -0600

    Actualización

---
+++ AnalisisProyecto/Analisis.Entidades/Almacen/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Analisis.Entidades.Almacen
+{
+    public class Categoria
+    {
+        public int idCategoria { get; set; }
+        
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Maximo 50 caràcteres")];
+        public string nombre { get; set; }
+        
+        [StringLength(256)]
+        public string descripcion { get; set; }
+    }
+}
---
+++ AnalisisProyecto/Analisis.Entidades/Usuarios/Usuario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Analisis.Entidades.Usuarios
+{
+    public class Usuario
+    {
+        public int idUsuario { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "El nombre de usuario debe de contener mínimo 8 y un máximo de 100 carácteres")]
+        public string nombre { get; set; }
+
+        public string apellido { get; set; }
+
+        public int tipoDocumento { get; set; }
+
+        public int numDocumento { get; set; }
+
+        [Required]
+        [StringLength (20, MinimumLength = 5, ErrorMessage = "Debe de ingresar por lo menos 5 carácteres")]
+        public string calle { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Debe de ingresar por lo menos 5 carácteres")]
+        public string avenida { get; set; }
+
+        
+        public int zona { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Debe de ingresar por lo menos 5 carácteres")]
+        public string colonia { get; set; }
+        [Required]
+        [StringLength(11, ErrorMessage = "Telefono debe de tener 11 dígitos")]
+        public string telenono { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Debe de ingresar por lo menos 5 carácteres")]
+
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression("")]
+
+        public string email { get; set; }
+
+        public string passwordHash { get; set; }
+
+        public string passwordSalt { get; set; }
+    }
+
+}
